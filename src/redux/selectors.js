fc768a6be7e76179206256f49cf0676bbeac123e
martynenkovDm <126678952+martynenkovDm@users.noreply.github.com>
2023-06-12 12:27:47 +0300

    First commit

---
+++ src/redux/selectors.js
@@ -0,0 +1,24 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+export const selectIsLoading = ({ contacts }) => contacts.isLoading;
+export const selectError = ({ contacts }) => contacts.error;
+export const selectFilter = ({ filter }) => filter;
+
+export const selectContacts = ({ contacts }) =>
+  [...contacts.items].sort((a, b) => a.name.localeCompare(b.name));
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    if (!filter) {
+      return contacts;
+    }
+    const normalizedFilter = filter.toLowerCase();
+    const filteredContacts = contacts.filter(
+      ({ name, phone }) =>
+        name.toLowerCase().trim().includes(normalizedFilter) ||
+        phone.trim().includes(normalizedFilter)
+    );
+    return filteredContacts;
+  }
+);
